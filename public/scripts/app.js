dd3af52a7d16e3cc7f0f17c4e99671eeb52974b5
Connor Leech <connorleech@gmail.com>
2014-10-12 22:56:09 -0700

    start over

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,17 @@
 'use strict';
 
 var app = angular.module('oakCrimeApp', [
-	'ui.router'
+	'ui.router',
+  'ui.bootstrap'
 ]);
 
 // routing
 app.config(function($stateProvider, $urlRouterProvider){    
   $stateProvider
-      .state("crimes", {
-          url: "/crimes",
+      .state("main", {
+          url: "/",
           controller: 'MainCtrl',
           templateUrl: 'views/main.html'
-      })
-      .state("policeBeats", {
-          url: "/policeBeats",
-          controller: 'PoliceBeatsCtrl',
-          templateUrl: 'views/policeBeats.html'
-      })
-      .state('awards', {
-        url: '/awards',
-        templateUrl: 'views/awards.html',
-        controller: 'AwardsCtrl'
-      })
-      .state('salaries', {
-        url: '/salaries',
-        templateUrl: 'views/salaries.html',
-        controller: 'SalariesCtrl'
       })
   
   $urlRouterProvider.otherwise("/");
--- server/routes.js
+++ server/routes.js
@@ -6,12 +6,22 @@ var router = express.Router();
 
 module.exports = function(app){
 
+	// API Routes
 	// only requests to /api/* will be sent to our "router"
 	app.use('/api', router);
 
-	// API Routes
+	// query string
 	router.get('/', function(req, res){
+		var year = parseInt(req.query.year);
+		var start = new Date(year, 0, 1);
+		var end = new Date(year, 11, 30);	// leap years..
 
+		Crime.find({ date: { $lte: end, $gt: start } }, function(err, crimes){
+			if(err) return err;
+
+			console.log(crimes);
+			res.json(crimes);
+		})
 	});
 
 	// Angular routing
